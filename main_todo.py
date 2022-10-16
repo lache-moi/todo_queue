c455503aa4a5286d2c062a83b0596f7df79cd0df
Lachlan Pham <44896700+lache-moi@users.noreply.github.com>
2022-10-16 19:16:14 +0400

    core functionality

---
+++ main_todo.py
@@ -0,0 +1,216 @@
+from unicodedata import category
+from todo_queue import Task, TodoQueue, TIME_FORMAT, TASK_CATEGORY_ALIASES
+import datetime
+from NicePrinter import title, bold, box
+import time
+import datetime
+import csv
+import os
+import sys
+
+"""
+Future instruction:
+    - change priority, cp
+    - rename: rn
+    - suggest random task for boredom
+    - search by category: sbc
+    - archive completed tasks:
+    - undo (advanced)
+    - retrieve from completed tasks
+        - add ids to completed tasks
+"""
+
+class Main:
+    def __init__(self, task_filename = "todo_queue.csv", log_filename = "completed_tasks.csv"):
+        self.task_filename = task_filename
+        self.log_filename = log_filename
+        self.q = TodoQueue()
+        
+        self.instructions = {
+            "add": (self.add, "a", "Add one task to the queue.",  ["[Optional[str]]: Name of task"]),
+            "complete": (self.complete, "c", "Complete a task at the front of the queue.", ["[Optional[int]]: Local id of task if not front of queue"]),
+            "delete": (self.delete, "del", "Delete (not complete) a task given a local id", ["[int]: Local id of task to delete"]),
+            "print": (lambda x: print(self.q), "p", "Print queue."),
+            "info": (lambda x: self.info(), "i", "Print available actions."),
+            "exit": (self.exit, "e", "Save queue and exit."),
+            "clear": (lambda x: os.system('clear'), "clear", 'Clear terminal.'),
+        }   
+        
+        self.start()
+        self.main_loop()
+    
+    def main_loop(self):
+
+        os.system('clear')
+        print(title("Let's get shit done!"))
+        print(self.q)
+        self.info()
+
+        aliases = {self.instructions[i][1]:i for i in self.instructions}
+
+        def parse_instructions(inp):
+            action = inp.split()[0]
+            if action in aliases:
+                action = aliases[action]
+            args = inp.split()[1:]
+            return action, args
+
+        valid_inputs = list(self.instructions.keys()) + list(aliases.keys())
+        while True:
+            action, args = self.get_input(type = "non-empty text", 
+                                out_text_override = "",
+                                input_test_override = lambda x: x.split()[0] in valid_inputs,
+                                input_conversion_override = lambda x: parse_instructions(x))
+            self.instructions[action][0](args)
+            print()
+
+    def exit(self, args):
+        self.save()
+        print("Successfully exit, have a swell day and go do drugs.")
+        exit()
+
+    def complete(self, args):
+        if args:
+            if len(args) == 1 and args[0].isnumeric() and int(args[0]) in self.q.ids:
+                completed_task = self.q.pull_given_id(int(args[0]))
+            else: 
+                print("Complete takes one numeric argument and it must match the local id of an existing task.")
+                return
+        else:
+            if self.q.empty():
+                print("There are no tasks to complete.")
+                return
+            else:
+                completed_task = self.q.pull_first()
+        self.log(completed_task)
+        print(self.q)
+        print(f"Successfully completed {completed_task.name} (id {completed_task.local_id}) at {datetime.datetime.now().strftime(TIME_FORMAT)}.")
+            
+    def delete(self, args):
+        if len(args) == 1 and args[0].isnumeric() and int(args[0]) in self.q.ids:
+            deleted_task = self.q.pull_given_id(int(args[0]))
+        else:
+            print("Delete takes one numeric agument and it must match the local id of an existing task.")
+            return
+        print(f"Successfully deleted {deleted_task.name} (id {deleted_task.local_id}) at {datetime.datetime.now().strftime(TIME_FORMAT)}.")
+
+            
+    
+    def add(self, args):
+        if args:
+            if len(args) == 1:
+                name = args[0]
+            else: 
+                print("Add only takes one argument.")
+                return
+        else:
+            name = self.get_input(type = "non-empty text", out_text_override = "Task name\n")
+        local_id = self.q.increment_counter()
+        priority = self.get_input(type = "int", 
+                                    out_text_override = "Priority [0:now | 1:day | 2:week | 3:month | 4:non-urgent]\n", 
+                                    input_test_override = lambda x: len(x) == 1 and x in '01234',
+                                    default_input="2")
+        category = self.get_input(type = "non-empty text", 
+                                    out_text_override = f"Category {TASK_CATEGORY_ALIASES} or manual input\n", 
+                                    input_conversion_override = lambda x: TASK_CATEGORY_ALIASES[x] if x in TASK_CATEGORY_ALIASES else x,
+                                    default_input="Misc")
+        time_created = datetime.datetime.now()
+        description = self.get_input(type = "text", 
+                                    out_text_override = f"Description\n",)
+        self.q.put(Task(local_id, name, priority, category, time_created, description))
+        print(self.q)
+
+    def info(self):
+        print(box("Possible actions"))
+        for i in self.instructions:
+            details = self.instructions[i]
+            print(bold(f"{i} ({details[1]})") + f" - {details[2]}")
+            if len(details) > 3:
+                [print("   " + arg) for arg in details[3]]
+            print()
+        
+
+
+    def start(self):
+        print(title("Welcome to your To-Do Queue", 90, '='))
+        print(f"To get started, would you like to use '{self.task_filename}' to retrieve and store existing tasks?")
+        if self.get_input(default_input="y"):
+            if os.path.exists(self.task_filename):
+                self.load()
+                print("Successfully loaded from file.")
+            else:
+                print(f"File doesn't exist. New file '{self.task_filename}' will be created after exiting.")
+        else:
+            print("Set new filename.")
+            self.task_filename = self.get_input(type="non-empty text")
+            # Check if file exists and set overload
+        print("Task file setup complete.\n")
+
+        print(f"Would you like to use '{self.log_filename}' to store completed tasks?")
+        if not self.get_input(default_input="y"):
+            self.log_filename = self.get_input(type="non-empty text")
+        
+        log_file_existed = os.path.exists(self.log_filename)
+        self.log_file = open(self.log_filename, 'a')
+        self.logger = csv.writer(self.log_file)
+        if not log_file_existed:
+            self.logger.writerow(Task.get_attribute_names() + ["time_completed"])
+            print("New logging file created. ", end="")
+        print("Task log setup complete.\n")
+
+    def save(self):
+        with open(self.task_filename, 'w') as outfile:
+            task_writer = csv.writer(outfile)
+            task_writer.writerow(Task.get_attribute_names())
+            for task in self.q:
+                task_writer.writerow(task.get_properties().values())
+        self.log_file.close()
+        
+    def log(self, task):
+        self.logger.writerow(list(task.get_properties().values()) + [datetime.datetime.now().strftime(TIME_FORMAT)])
+
+    def load(self):
+        # Check valid file
+        with open(self.task_filename, 'r') as infile:
+            task_reader = csv.reader(infile)
+            next(task_reader)
+            for row in task_reader:
+                new_task = Task(
+                    self.q.increment_counter(), 
+                    row[2], row[3], row[4], 
+                    datetime.datetime.strptime(row[1], TIME_FORMAT),
+                    row[5],
+                    datetime.datetime.strptime(row[6], TIME_FORMAT) if row[6] else None,
+                )
+                self.q.put(new_task)
+
+    def get_input(self, type = "yes-no", default_input = None, invalid_input_text = "Invalid input, please try again.",
+                    out_text_override = None, input_test_override = None, input_conversion_override = None):
+        # overrides for input test and input conversion
+
+        input_types =\
+        {
+            "yes-no": ("[y/n] ", lambda x: x in ["y", "n"], lambda x: x == "y"),
+            "int": ("[int] ", lambda x: x.isnumeric(), lambda x: int(x)),
+            "non-empty text": ("[str] ", lambda x: bool(x), lambda x: x),
+            "text": ("[str] ", lambda x: True, lambda x: x),
+        }
+
+        out_text, input_test, input_conversion = input_types[type]
+        out_text = out_text_override if out_text_override != None else out_text
+        input_test = input_test_override if input_test_override else input_test
+        input_conversion = input_conversion_override if input_conversion_override else input_conversion
+
+        if default_input:
+            out_text += f"(default {default_input})"
+        out_text += ": "
+
+        while not input_test(inp := input(out_text).lower()):
+            if default_input and not inp:
+                return input_conversion(default_input)
+            print(invalid_input_text)
+
+        return input_conversion(inp)
+
+if __name__ == "__main__":
+    Main()
---
+++ todo_queue.py
@@ -0,0 +1,117 @@
+import json
+import datetime
+from NicePrinter import table
+
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+TASK_CATEGORY_ALIASES = {
+    "a": "Admin",
+    "c": "Cleaning",
+    "hw": "Homework",
+}
+
+class Task:
+    def __init__(self, local_id: int, name: str, priority: int, category: str, time_created: datetime.datetime, description: str = None, set_urgent: datetime.datetime = None):
+        self.local_id = local_id
+        self.time_created = time_created
+        self.name = name
+        self.priority = priority
+        self.category = category
+        self.description = description
+        self.set_urgent = set_urgent
+        self.prev = None
+        self.next = None
+
+    def update_priority(self, new_priority: int):
+        self.priority = new_priority
+
+    def get_basic_info(self):
+        info = self.get_properties()
+        info["priority"] = f"P{info['priority']}"
+        for attr in info:
+            info[attr] = str(info[attr]) if info[attr] else ""
+
+        return list(info.values())
+
+    def sort_priority(self):
+        return str(self.priority) + datetime.datetime.now().strftime(TIME_FORMAT)
+         
+    def __str__(self):
+        return json.dumps(self.get_properties())
+
+    def get_properties(self):
+        return {
+            "local_id": self.local_id,
+            "time_created": self.time_created.strftime(TIME_FORMAT),
+            "name": self.name,
+            "priority": self.priority,
+            "category": self.category,
+            "description": self.description,
+            "set_urgent": self.set_urgent.strftime(TIME_FORMAT) if self.set_urgent else None,
+        }
+
+    @staticmethod
+    def get_readable_attribute_names():
+        return ["Local Id", "Time Created", "Name", "Priority", "Category", "Description", "Set Urgent"]
+    @staticmethod
+    def get_attribute_names():
+        return [attribute.lower().replace(" ", "_") for attribute in Task.get_readable_attribute_names()]
+
+class TodoQueue:
+    def __init__(self, infile: str = None):
+        self.local_id_counter = 0
+        self.ids = {}
+        self.head = Task(None, None, None, None, None, None)
+        self.tail = Task(None, None, None, None, None, None)
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def increment_counter(self):
+        self.local_id_counter += 1
+        return self.local_id_counter
+    
+    def empty(self):
+        return not self.ids
+
+    def insert_task_before(self, node, new_task):
+        new_task.next, new_task.prev = node, node.prev
+        node.prev.next = new_task
+        node.prev = new_task
+        return new_task
+
+    def remove_task(self, task):
+        self.ids.pop(task.local_id)   
+        task.prev.next, task.next.prev = task.next, task.prev
+        return task        
+
+    def put(self, new_task):
+        self.ids[new_task.local_id] = new_task
+        for node in self:
+            if new_task.sort_priority() < node.sort_priority():
+                return self.insert_task_before(node, new_task)
+        return self.insert_task_before(self.tail, new_task)
+
+    def pull_first(self):
+        if not self.empty():
+            return self.remove_task(self.head.next)
+    
+    def pull_given_id(self, local_id):
+        if task_to_remove := self.ids.get(local_id):
+            return self.remove_task(task_to_remove)
+
+    def __str__(self):
+        output = [Task.get_readable_attribute_names()]
+        [output.append(task.get_basic_info()) for task in self]
+        return table(output, centered = True) + "\n"
+
+    def __iter__(self):
+        self.curr_node = self.head
+        return self
+    
+    def __next__(self):
+        curr_task = self.curr_node.next
+        if curr_task.name:
+            self.curr_node = self.curr_node.next
+            return curr_task
+        raise StopIteration
+
+
+    
